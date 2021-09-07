7678b30b0de101d876da4099c65f2a297338f88b
Jader Martins <jadermcs94@gmail.com>
2021-09-07 14:56:32 -0300

    response modeling

--- train.py
+++ train.py
@@ -23,23 +23,50 @@ datasets = load_dataset("parquet", data_files={"train": train_files,
                                                "validation": validation_files})
 datasets = datasets.filter(lambda x: x['text'] is not None and x['reply'] is not None)
 
-def tokenize_function(examples):
-    result = tokenizer(examples["text"],
-                       padding="max_length", truncation=True, max_length=128)
-    result['labels'] = result["input_ids"].copy()
-    return result
+special_tokens = ["<sos_u>", "<eos_u>", "<sos_r>", "<eos_r>"]
+tokenizer.add_special_tokens({'additional_special_tokens': special_tokens})
 
-tokenized_datasets = datasets.map(
+def add_tokens(examples):
+    examples["text"] = "<sos_u> "+examples["text"]+" <eos_u>"
+    examples["reply"] = "<sos_r> "+examples["reply"]+" <eos_r>"
+    examples["intext"] = examples["text"] + examples["reply"]
+    return examples
+
+parsed_datasets = datasets.map(
+    add_tokens,
+    num_proc=4,
+    remove_columns=["id", "text", "reply"])
+
+def tokenize_function(examples):
+    return tokenizer(examples["intext"])
+
+tokenized_datasets = parsed_datasets.map(
     tokenize_function,
     num_proc=4,
     batched=True,
-    batch_size=128,
-    remove_columns=["text", "reply"])
+    remove_columns=["intext", "__index_level_0__"])
+
+def group_texts(examples):
+    block_size = 128
+    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    total_length = (total_length // block_size) * block_size
+    result = {
+        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+        for k, t in concatenated_examples.items()
+    }
+    result["labels"] = result["input_ids"].copy()
+    return result
+
+lm_datasets = tokenized_datasets.map(
+    group_texts,
+    batched=True,
+    num_proc=4)
 
 training_args = TrainingArguments(
     "test-clm",
     evaluation_strategy="epoch",
-    per_device_train_batch_size=64,
+    per_device_train_batch_size=32,
     learning_rate=2e-5,
     weight_decay=0.01,
     warmup_steps=2000,
@@ -50,8 +77,8 @@ training_args = TrainingArguments(
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=tokenized_datasets["train"],
-    eval_dataset=tokenized_datasets["validation"],
+    train_dataset=lm_datasets["train"],
+    eval_dataset=lm_datasets["validation"],
 )
 
 trainer.train()
